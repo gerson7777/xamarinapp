edce61278eecfed41c8dffc13aa04dca642c5ade
Gerson Bernardo Gonzalez Pérez <gersunone@hotmail.com>
2017-09-19 23:07:44 -0500

    segundoComit

---
+++ xamarinapp/xamarinapp/infraestructure/InstanceLacator.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace xamarinapp.infraestructure
+{
+    using ViewModel;
+    public class InstanceLacator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLacator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
